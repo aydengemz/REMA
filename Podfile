c2b474ecea406ece45613a465c262dbb764f7d17
Ayden Xu <aydenxu@xus-macbook.local>
2018-10-07 14:15:10 -0400

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+use_frameworks!
+
+target 'REMA' do
+  # Pods for Simple Chat
+  pod 'JSQMessagesViewController'
+ 
+  # Watson Pods
+  pod 'IBMWatsonAssistantV1'
+  pod 'IBMWatsonSpeechToTextV1'
+  pod 'IBMWatsonTextToSpeechV1'
+  pod 'IBMWatsonConversationV1'
+  #pod 'IBMWatsonDiscoveryV1'
+  #pod 'IBMWatsonLanguageTranslatorV3', '~> 0.34.0'
+  #pod 'IBMWatsonNaturalLanguageClassifierV1', '~> 0.34.0'
+  #pod 'IBMWatsonNaturalLanguageUnderstandingV1', '~> 0.34.0'
+  #pod 'IBMWatsonPersonalityInsightsV3', '~> 0.34.0'
+  #pod 'IBMWatsonToneAnalyzerV3', '~> 0.34.0'
+  #pod 'IBMWatsonVisualRecognitionV3', '~> 0.34.0'
+end
+
+
+
---
+++ REMA/Credentials.swift
@@ -0,0 +1,25 @@
+/**
+ * Copyright IBM Corporation 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+ 
+public struct Credentials {
+    public static let AssistantUsername = "3e31e930-f078-4f2c-aa28-6d5636f1f490"
+    public static let AssistantPassword = "cMQ2ykb2Dpo4"
+    public static let AssistantWorkspace = "22b5b09d-1d08-47d7-a897-2a799af2013c"
+    public static let SpeechToTextUsername = "fdb57f41-7db1-4350-8650-b27ed3d1e283"
+    public static let SpeechToTextPassword = "1nHYLlsBRDQj"
+    public static let TextToSpeechUsername = "e4f731d7-d234-4544-83eb-fad7c9cb24a8"
+    public static let TextToSpeechPassword = "c2PiumHKTLGQ"
+}
